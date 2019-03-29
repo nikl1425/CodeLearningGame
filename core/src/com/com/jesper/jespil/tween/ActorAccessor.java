8b8393dca91fb3e6093c4c559017a7e58ba01beb
jaco4336 <jjfivel@gmail.com>
2019-03-29 15:24:34 +0100

    afdad

--- core/src/com/com/jesper/jespil/tween/ActorAccessor.java
+++ core/src/com/com/jesper/jespil/tween/ActorAccessor.java
@@ -1,7 +1,8 @@
 package com.com.jesper.jespil.tween;
 
-import aurelienribon.tweenengine.TweenAccessor;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import aurelienribon.tweenengine.TweenAccessor;
 
 public class ActorAccessor implements TweenAccessor<Actor> {
 
